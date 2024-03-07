1cc39c659d779afe9113c611043983323b3852c0
권민재 <162289516+kwonminjae5700@users.noreply.github.com>
2024-03-07 19:12:18 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n1=1, n2=1;
+    
+    while(1) {
+        scanf("%d %d", &n1, &n2);
+        if(n1==0 && n2==0) {
+            break;
+        }
+        printf("%d\n", n1+n2);   
+    }
+}
