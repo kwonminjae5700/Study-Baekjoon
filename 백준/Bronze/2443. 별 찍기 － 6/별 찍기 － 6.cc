bb9ec963a2b9ad6807a2dd2ff70c478d8b8dba43
권민재 <162289516+kwonminjae5700@users.noreply.github.com>
2024-04-03 11:19:26 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+
+    for(int i=N;i>=1;i--) {
+        for(int l=1;l<=N-i;l++) {
+            printf(" ");
+        }
+        
+        for(int j=i*2-1;j>=1;j--) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
