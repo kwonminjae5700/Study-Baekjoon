09294d35dd794a1a163782a8df41b05c336c1409
권민재 <162289516+kwonminjae5700@users.noreply.github.com>
2024-06-03 12:00:06 +0900

    [Bronze II] Title: 방 배정하기, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- C99/백준/Bronze/14697. 방 배정하기/방 배정하기.c
+++ C99/백준/Bronze/14697. 방 배정하기/방 배정하기.c
@@ -5,11 +5,13 @@ int main() {
 
     scanf("%d %d %d %d", &A, &B, &C, &N);
 
-    N%=C;
-    N%=B;
-    N%=A;
-
-    if(N==0) result=1;
+    for(int i = 0;i<(N/A)+1;i++){
+        for(int j = 0;j<(N/B)+1;j++){
+            for(int l = 0;l<(N/C)+1;l++){
+                if((i*A + j*B + l*C)==N) result=1;
+            }
+        }
+    }
 
     printf("%d", result);
 
