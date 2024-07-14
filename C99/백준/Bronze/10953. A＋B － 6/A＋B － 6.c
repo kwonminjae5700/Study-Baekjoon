dbc87a37bf6a791a4a3949a99bafbabb7f21d38e
권민재 <162289516+kwonminjae5700@users.noreply.github.com>
2024-07-15 08:59:38 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10953. A＋B － 6/A＋B － 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, N;
+
+    scanf("%d", &N);
+
+    for(int i=0;i<N;i++) {
+        scanf("%d, %d", &A, &B);
+
+        printf("%d\n", A+B);
+    }
+    
+    return 0;
+}
