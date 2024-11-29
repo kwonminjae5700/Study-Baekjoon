6fecaff89d5aae4404fb71bf9927c2fdebd86151
권민재 <162289516+kwonminjae5700@users.noreply.github.com>
2024-11-30 00:35:07 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 88 ms, Memory: 9592 KB -BaekjoonHub

---
+++ C99/백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int palindrome(int num) {
+    int arr[10], idx=0;
+    while(num) {
+        arr[idx++] = num%10;
+        num/=10;
+    }
+
+    int front=0, back=idx-1, count=0;
+
+    for(int i=0;i<idx/2;i++) {
+        if(arr[front++] == arr[back--]) count++;
+    }
+
+    if(count == idx/2) return 1;
+    else return 0;
+}
+
+int main() {
+    int N, flag=0;
+
+    scanf("%d", &N);
+    int num[2000001]={0}, prime[200001], idx=0;
+
+    for(int i=2;i<=2000001;i++) {
+        for(int j=2;i*j<=2000001;j++) num[i*j] = 1;
+    }
+
+    for(int i=2;i<=2000001;i++) {
+        if(!num[i] && i>=N) prime[idx++] = i;
+
+    }
+
+    if(N<=7) printf("%d", prime[0]);
+    else {
+        idx = 0;
+
+        while(1) {
+            if(palindrome(prime[idx])) {
+                printf("%d ", prime[idx]);
+                flag = 1;
+                break;
+            }
+
+            idx++;
+        }
+    }
+
+    return 0;
+}
